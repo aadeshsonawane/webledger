f37cc780c1529e5942b07f409b5eb43d8947adcf
aadeshsonawane <aadeshsonawane307@gmail.com>
2026-06-17 10:28:04 +0530

    add day2 folder with props project

---
+++ day2/props/src/App.vue
@@ -0,0 +1,13 @@
+<script setup>
+import Child from './components/Child.vue';
+</script>
+
+
+<template>
+  <h1>Props</h1>
+  
+  <Child name="aadesh"/>  
+ 
+</template>
+
+<style scoped></style>
---
+++ day2/props/src/components/Child.vue
@@ -0,0 +1,14 @@
+<script setup>
+
+const props = defineProps({
+    name : String
+})
+
+
+</script>
+
+<template>
+  <h1>hello my name is{{ name }}</h1>
+</template>
+
+<style scoped></style>
