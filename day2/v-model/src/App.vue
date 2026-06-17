c7a9f1770294b184573b670df1da38727c26fe1c
aadeshsonawane <aadeshsonawane307@gmail.com>
2026-06-17 12:01:49 +0530

    add v-model project

---
+++ day2/v-model/src/App.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ref } from 'vue';
+
+
+const name = ref("")
+const showMe = ref("")
+
+const showName = ()=>{
+  showMe.value = name.value
+}
+
+</script>
+
+<template>
+  <h1>You did it!</h1>
+  <input type="text" placeholder="name" v-model="name">
+   <button @click="showName">add me</button>
+   
+  <p>{{ showMe }}</p>
+</template>
+
+<style scoped></style>
