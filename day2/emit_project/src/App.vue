63251bb0d15f7257da8215f9b56610a4872c4306
aadeshsonawane <aadeshsonawane307@gmail.com>
2026-06-17 14:22:51 +0530

    add emit_project

---
+++ day2/emit_project/src/App.vue
@@ -0,0 +1,18 @@
+<script setup>
+import Add from "./components/Add.vue"
+
+
+const addNumbers =(a ,b)=>{
+  console.log(a+b)
+
+}
+</script>
+
+<template>
+  <h1>You did it!</h1>
+  <p>
+    <Add @add="addNumbers"/>
+  </p>
+</template>
+
+<style scoped></style>
---
+++ day2/emit_project/src/components/Add.vue
@@ -0,0 +1,28 @@
+<script setup>
+import { ref } from 'vue'
+
+const emit = defineEmits(['add'])
+
+const num1 = ref()
+const num2 = ref()
+
+
+
+const addNmber = ()=>{
+    emit("add",Number(num1.value), Number(num2.value) )
+}
+
+
+</script>
+
+<template>
+
+    <input type="text" v-model="num1" placeholder="number">
+    <input type="text" v-model="num2" placeholder="number">
+    
+
+
+    <button @click="addNmber">add</button>
+
+
+</template>
