8247f5f2b0b0b67ef521ef42f7b9b8d75dff8e35
aadeshsonawane <aadeshsonawane307@gmail.com>
2026-06-18 10:40:20 +0530

    add onMounted function

--- day1/todoList/src/App.vue
+++ day1/todoList/src/App.vue
@@ -1,23 +1,41 @@
 <script setup>
-import { ref } from 'vue'
+import { ref, onMounted } from 'vue'
 
 const title = ref('')
 const date = ref('')
 
 const tasks = ref([])
 
+onMounted(() => {
+  const savedTasks = localStorage.getItem("tasks")
+
+  if (savedTasks) {
+    tasks.value = JSON.parse(savedTasks)
+  }
+})
+
+
+// Add task function
+
 function addTask() {
-  tasks.value.push({
-    Title: title.value,
-    Date: date.value
-  })
+  const newTask = {
+  id: Date.now(),
+  title: title.value,
+  date: date.value
+}
+tasks.value.push(newTask)
+
+localStorage.setItem("tasks", JSON.stringify(tasks.value))
 
   title.value = ""
   date.value = ""
 }
 
+// Delete Task Function
+
 function deleteTask(id) {
   tasks.value = tasks.value.filter(task => task.id !== id)
+  localStorage.setItem("tasks", JSON.stringify(tasks.value))
 }
 
 
