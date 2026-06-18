021a41a4093e5f40aa1354a85125650b5ee8a3d1
aadeshsonawane <aadeshsonawane307@gmail.com>
2026-06-18 15:34:28 +0530

    input

---
+++ day3/vue_project/src/App.vue
@@ -0,0 +1,43 @@
+<script setup>
+
+// import Header from './components/Header.vue';
+// import Counter from './components/Counter.vue';
+
+import { ref } from 'vue';
+  const name = ref()
+const password = ref()
+
+
+const submittedName = ref('')
+const submittedPassword = ref('')
+
+const submit=()=> {
+  submittedName.value = name.value
+  submittedPassword.value = password.value
+
+}
+
+</script>
+
+<template>
+
+  <input type="text" placeholder="name" v-model="name">
+  <br>
+  <br>
+  <input type="text" placeholder="password" v-model="password">
+  <br>
+  <br>
+  <button @click="submit">submit</button>
+
+
+  <p>{{ submittedName }}</p>
+  <p>{{ submittedPassword }}</p>
+ 
+</template>
+
+
+
+<style scoped>
+
+
+</style>
---
+++ day3/vue_project/src/components/Header.vue
@@ -0,0 +1,19 @@
+<script>
+
+
+</script>
+
+<template>
+
+    <h1>i am header </h1>
+    <!-- {{ var a = 1 }} -->
+
+
+</template>
+
+<style scoped>
+
+h1{
+    color: red;
+}
+</style>
