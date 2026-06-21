dcf49e29299716ed11b06ba73402ad01668e51ea
aadeshsonawane <aadeshsonawane307@gmail.com>
2026-06-21 08:33:19 +0530

    without expose share child function with parent

---
+++ day4/withOutExpos/src/App.vue
@@ -0,0 +1,21 @@
+<script setup>
+import Child from './components/Child.vue'
+import { emitter } from './components/eventBus.js'
+
+const callMe = () => {
+  emitter.emit('runHello')
+}
+</script>
+
+<template>
+  <h1>I am Parent</h1>
+
+  <Child />
+
+  <button @click="callMe">
+    Say Hello
+  </button>
+</template>
+
+<style scoped>
+</style>
---
+++ day4/withOutExpos/src/components/Child.vue
@@ -0,0 +1,34 @@
+<script setup>
+import { emitter } from './eventBus'
+import { ref } from 'vue'
+
+const str = ref("")
+
+const sayHello = () => {
+
+  str.value = "hello from chiild"
+
+  // console.log('Hello from Child')
+}
+
+emitter.on('runHello', () => {
+  sayHello()
+})
+
+
+// onMounted(() => {
+//   emitter.on('runHello', handleRunHello)
+// })
+
+// onUnmounted(() => {
+//   emitter.off('runHello', handleRunHello)
+// })
+</script>
+
+<template>
+  <h1>Hello from Child</h1>
+  <p>{{ str }}</p>
+</template>
+
+<style scoped>
+</style>
